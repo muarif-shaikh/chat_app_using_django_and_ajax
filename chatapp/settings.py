ad9d18889a20556f223c8fd8bb636a73d1f691cb
Muarif Shaikh <shaikhmuarif@gmail.com>
2024-08-07 01:57:32 +0530

    Initial Commit

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['web-production-28e7.up.railway.app']
 
 
 # Application definition
