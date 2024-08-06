910a5cc23b53332441b2ae953919698c48789cb5
Muarif Shaikh <shaikhmuarif@gmail.com>
2024-08-07 02:10:25 +0530

    Initial Commit

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -75,8 +75,9 @@ WSGI_APPLICATION = 'chatapp.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.config(default=config('DATABASE_URL'))
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
