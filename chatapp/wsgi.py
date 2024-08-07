5e7c1f6588be8e38fc9d0d36901afb644f094a84
Muarif Shaikh <shaikhmuarif@gmail.com>
2024-08-07 10:15:46 +0530

    Initial Commit

--- chatapp/wsgi.py
+++ chatapp/wsgi.py
@@ -1,7 +1,12 @@
 import os
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling, MediaCling
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatapp.settings')
 
-application = Cling(MediaCling(get_wsgi_application()))
+# Import BASE_DIR from settings.py
+from chatapp.settings import BASE_DIR
+
+application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
+application.add_files(os.path.join(BASE_DIR, 'media'), prefix='media/')
