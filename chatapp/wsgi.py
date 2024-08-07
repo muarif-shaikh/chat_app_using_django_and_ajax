baa110609c58b6b7416572c88cc50e8931f51bd6
Muarif Shaikh <shaikhmuarif@gmail.com>
2024-08-07 10:04:37 +0530

    Initial Commit

--- chatapp/wsgi.py
+++ chatapp/wsgi.py
@@ -1,16 +1,7 @@
-"""
-WSGI config for chatapp project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling, MediaCling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatapp.settings')
 
-application = get_wsgi_application()
+application = Cling(MediaCling(get_wsgi_application()))
